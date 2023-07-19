df67709c116d597631fcd8e0ad064c29a0c8a672
ats147 <ats14795@gmail.com>
2023-07-19 19:12:22 +0530

    First Commit

---
+++ src/auth/strategies/at.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+type JwtPayLoad={
+    sub:string;
+    email:string;
+}
+
+@Injectable()
+export class AtStrategy extends PassportStrategy(Strategy,'jwt'){
+    constructor(){
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey:'at-strategy',
+        });
+    }
+
+    validate(payload:JwtPayLoad){
+        return payload;
+    }
+}
